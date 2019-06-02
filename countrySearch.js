308d0fd60a50b59f06627e7ab28b42c9f664c70d
Bartek Bugała <bartek.bugala@gmail.com>
2019-06-02 15:06:07 +0200

    modified:   countrySearch.js

--- countrySearch.js
+++ countrySearch.js
@@ -1,41 +1,39 @@
-'use strict';
-(function () {
+"use strict";
+(function() {
+  const searchButton = document.getElementById("search");
+  const restCountriesUrl = "https://restcountries.eu/rest/v2/name/";
 
-    const searchButton = document.getElementById('search');
-    const restCountriesUrl = 'https://restcountries.eu/rest/v2/name/';
+  let countriesList = document.getElementById("countries");
 
-    let countriesList = document.getElementById('countries');
+  searchButton.addEventListener("click", searchCountries);
 
-    searchButton.addEventListener('click', searchCountries);
+  document.getElementById("country-name").addEventListener("keydown", function(event) {
+    // Code 13 = Enter
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      searchButton.click();
+    }
+  });
 
-    document.getElementById('country-name').addEventListener('keydown', function (event) {
-        // Code 13 = Enter
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            searchButton.click();
-        }
+  function searchCountries() {
+    let countryName = document.getElementById("country-name").value;
+    if (!countryName.length) countryName = "Poland";
+
+    fetch(restCountriesUrl + countryName)
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(showCountriesList);
+  }
+
+  function showCountriesList(resp) {
+    countriesList.innerHTML = "";
+    resp.forEach(function(item) {
+      let liEl = document.createElement("li");
+      liEl.classList.add("row");
+      liEl.innerHTML = `<span class="col-6">${item.name}</span>`;
+      liEl.innerHTML += `<span class="col-6"><img class="country-flag"src="${item.flag}" alt="flag"></span>`;
+      countriesList.appendChild(liEl);
     });
-
-    function searchCountries() {
-        let countryName = document.getElementById('country-name').value;
-        if (!countryName.length) countryName = 'Poland';
-
-        fetch(restCountriesUrl + countryName)
-            .then(function (resp) {
-                return resp.json();
-            })
-            .then(showCountriesList);
-    }
-
-    function showCountriesList(resp) {
-        countriesList.innerHTML = '';
-        resp.forEach(function(item){
-            let liEl = document.createElement('li');
-            liEl.classList.add('row');
-            liEl.innerHTML = `<span class="col-6">${item.name}</span>`;
-            liEl.innerHTML += `<span class="col-6"><img class="country-flag"src="${item.flag}" alt="flag"></span>`;
-            countriesList.appendChild(liEl);
-        });
-    }
-
+  }
 })();
